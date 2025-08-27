e21a1a3f53a16d18e016bb6c590dc0d5efdc5851
Nympicus <emiryildirimemir18@gmail.com>
2025-08-27 12:06:29 +0300

    added go

---
+++ pi.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func leibnizPi(n int) float64 {
+    pi := 0.0
+    sign := 1.0
+    for i := 0; i < n; i++ {
+        pi += sign / float64(2*i + 1)
+        sign = -sign
+    }
+    return pi * 4
+}
+func main() {
+    fmt.Println(leibnizPi(20000000000))
+}
