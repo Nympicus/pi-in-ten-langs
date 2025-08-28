15568814c8c68a4467b02a97d6686d171b294d28
Nympicus <emiryildirimemir18@gmail.com>
2025-08-28 13:19:45 +0300

    added rust

---
+++ pi.rs
@@ -0,0 +1,14 @@
+fn leibniz_pi(n: i64) -> f64 {
+    let mut pi: f64 = 0.0;
+    let mut sign: f64 = 1.0;
+    for i in 0..n {
+        pi += sign / (2.0 * i as f64 + 1.0); 
+        sign = -sign;
+    }
+    pi * 4.0
+}
+
+fn main() {
+    let result = leibniz_pi(20000000000);
+    println!("{}", result);
+}
